34d8c6710a3256de00a99819505eeae874428e9f
Faridat <faridatabu@yahoo.com>
2022-07-15 14:55:15 +0100

    I edited it

--- src/main/java/com/student/repository/Result_repository.java
+++ src/main/java/com/student/repository/Result_repository.java
@@ -4,10 +4,78 @@
  */
 package com.student.repository;
 
+import com.student.database.ConnectionDatabase;
+import com.student.model.Results;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+
 /**
  *
  * @author EKUNSAJUOLA
  */
 public class Result_repository {
-    
+
+    EntityManager em = ConnectionDatabase.getEntityManager();
+
+    public Results createStudentResults(long id, String student, String subject) {
+
+        EntityTransaction txn = em.getTransaction();
+
+        txn.begin();
+
+        Results rs = new Results();
+
+        rs.setId(id);
+//        rs.setStudent(student);
+//        rs.setSubjects(subjects);
+
+        em.persist(rs);
+        txn.commit();
+        return rs;
+
+    }
+
+    public Results findStudentResultByName(String student) {
+
+        Query myQuery = em.createNamedQuery("SELECT * FROM Results WHERE Results = " + student);
+
+        Results rs = (Results) myQuery.getSingleResult();
+
+        return rs;
+
+    }
+
+    public Results updateResultByName(String student) {
+
+        Query myQuery = em.createNamedQuery("UPDATE RESULTS WHERE Results =" + student);
+
+        Results rs = (Results) myQuery.getSingleResult();
+
+        return rs;
+    }
+
+    public Results updateResultBySubject(String subject) {
+
+        Query myQuery = em.createNamedQuery("UPDATE RESULTS WHERE Results =" + subject);
+
+        Results rs = (Results) myQuery.getSingleResult();
+
+        return rs;
+    }
+
+    public Results deleteResultById(long id) {
+
+        Results rs = em.find(Results.class, id);
+
+        EntityTransaction txn = em.getTransaction();
+
+        txn.begin();
+
+        em.remove(rs);
+
+        txn.commit();
+
+        return rs;
+    }
 }
--- src/main/java/com/student/repository/Subject_repository.java
+++ src/main/java/com/student/repository/Subject_repository.java
@@ -4,10 +4,53 @@
  */
 package com.student.repository;
 
+import com.student.database.ConnectionDatabase;
+import com.student.model.Subjects;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
 /**
  *
  * @author EKUNSAJUOLA
  */
 public class Subject_repository {
+
+    EntityManager em = ConnectionDatabase.getEntityManager();
+
+    public Subjects createSubject(String subjectName, String student) {
+
+        EntityTransaction txn = em.getTransaction();
+
+        txn.begin();
+
+        Subjects sb = new Subjects();
+        
+       // sb.setStudent(student);
+        sb.setSubjectName(subjectName);
+        
+        em.persist(sb);
+        txn.commit();
+        return sb;
+    }
     
+//    public Subjects getSubjectByName (String subjectName){
+//        
+//        Query myQuery = em.createNamedQuery("")
+//    }
+    public Subjects deleteSubjects (String subjectName){
+        
+         Subjects sb = em.find(Subjects.class, subjectName);
+
+        EntityTransaction txn = em.getTransaction();
+
+        txn.begin();
+
+        em.remove(sb);
+
+        txn.commit();
+
+        return sb;
+    }
+    
+   
 }
